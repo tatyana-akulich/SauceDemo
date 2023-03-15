418653f6c35594de4bfc4fb3424cbd4f459943d4
Tatyana Akulich <t.n.akulich@gmail.com>
2023-03-15 21:08:31 +0300

    added tests

--- src/test/java/by/teachmeskills/BaseTest.java
+++ src/test/java/by/teachmeskills/BaseTest.java
@@ -4,6 +4,8 @@ import by.teacmeskills.page.LoginPage;
 import by.teacmeskills.page.ProductsPage;
 import com.github.javafaker.Faker;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -12,16 +14,24 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import java.time.Duration;
-
+@Log4j2
 public class BaseTest {
     protected WebDriver driver;
     protected Faker faker;
 
     @BeforeClass
     public void setUp(ITestContext testContext) {
+        String headlessOnJenkins = System.getenv("HEADLESS");
+        log.info("Environmental variable from Jenkins - {}", headlessOnJenkins);
+        boolean isHeadless = false;
+        if (StringUtils.isNotEmpty(headlessOnJenkins)) {
+            isHeadless = Boolean.parseBoolean(headlessOnJenkins);
+        }
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");
+        if (isHeadless) {
+            options.addArguments("--headless=new");
+        }
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
 

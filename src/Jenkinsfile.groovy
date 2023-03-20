02136112cea151e82e87fbf5e45563e85f22086d
Tatyana Akulich <t.n.akulich@gmail.com>
2023-03-20 19:54:49 +0300

    master - added Jenkinsfile

---
+++ src/Jenkinsfile.groovy
@@ -0,0 +1,36 @@
+pipeline {
+    agent any
+
+    tools {
+        // Install the Maven version configured as "M3" and add it to the path.
+        maven "3.8.6"
+    }
+
+    stages {
+        stage('Build') {
+            steps {
+                // Get some code from a GitHub repository
+                git 'https://github.com/tatyana-akulich/SauceDemo.git'
+
+                // Run Maven on a Unix agent.
+                //sh "mvn -Dmaven.test.failure.ignore=true clean package"
+
+                // To run Maven on a Windows agent, use
+                bat "mvn -DsuiteXMLFile=src/test/resources/smoke.xml clean test"
+            }
+        }
+        stage('reports') {
+            steps {
+                script {
+                    allure([
+                     includeProperties: false,
+                     jdk: '',
+                     properties: [],
+                     reportBuildPolicy: 'ALWAYS',
+                     results: [[path: 'target/allure-results']]
+                     ])
+                }
+            }
+        }
+    }
+}
